dd6b6d1a8bae9352356e4b8934541c68d261b28a
Morgunov Evgenii <morgunove87@gmail.com>
2024-11-09 17:55:40 -0500

    add more symbols for game logic in Main.java

--- src/bullscows/Main.java
+++ src/bullscows/Main.java
@@ -7,16 +7,19 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please, enter the secret code's length: ");
         int length = scanner.nextInt();
 
-        if (length > 10) {
-            System.out.println("Error: can't generate a secret number with a length of " + length + " because there aren't enough unique digits.");
+        System.out.print("Input the number of possible symbols in the code: ");
+        int symbols = scanner.nextInt();
+
+        if (length > symbols || length > 36) {
+            System.out.println("Error: can't generate a secret number with a length of " + length + " and " + symbols + " possible symbols.");
         } else {
-            String secretCode = generateSecretCode(length);
+            String secretCode = generateSecretCode(length, symbols);
+            System.out.println("The secret is prepared: " + "*".repeat(length) + " (0-9, a-" + (char) ('a' + symbols - 11) + ").");
             System.out.println("Okay, let's start a game!");
 
             int turn = 1;
@@ -40,18 +43,16 @@ public class Main {
         }
     }
 
-    private static String generateSecretCode(int length) {
+    private static String generateSecretCode(int length, int symbols) {
         StringBuilder secretCode = new StringBuilder();
         Set<Character> usedDigits = new HashSet<>();
         Random random = new Random();
 
-        while(secretCode.length() < length) {
-            int digit = random.nextInt(10);
-            char digitChar = (char) (digit + '0');
+        String possibleSymbols = "0123456789abcdefghijklmnopqrstuvwxyz".substring(0, symbols);
 
-            if (secretCode.isEmpty() && digit == 0) {
-                continue;
-            }
+        while(secretCode.length() < length) {
+            char digitChar = possibleSymbols.charAt(random.nextInt(symbols));
+
             if(!usedDigits.contains(digitChar)) {
                 secretCode.append(digitChar);
                 usedDigits.add(digitChar);
