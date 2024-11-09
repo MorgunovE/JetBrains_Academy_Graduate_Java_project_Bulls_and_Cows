72451b68a909e02e33fc8335fc86d792fa573dcc
Morgunov Evgenii <morgunove87@gmail.com>
2024-11-09 15:24:28 -0500

    add generate method for secretCode in Main.java

--- src/bullscows/Main.java
+++ src/bullscows/Main.java
@@ -1,19 +1,52 @@
 package bullscows;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        String secretCode = "9305";
+
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the secret code: ");
+        int length = scanner.nextInt();
 
-        System.out.print("Enter your guess: ");
-        String guess = scanner.nextLine();
+        if (length > 10) {
+            System.out.println("Error: can't generate a secret number with a length of " + length + " because there aren't enough unique digits.");
+        } else {
+            String secretCode = generateSecretCode(length);
+            System.out.println("The random secret number is " + secretCode + ".");
+        }
+    }
 
+    private static String generateSecretCode(int length) {
+        StringBuilder secretCode = new StringBuilder();
+        Set<Character> usedDigits = new HashSet<>();
+
+        while(secretCode.length() < length) {
+            long pseudoRandomNumber = System.nanoTime();
+            String pseudoRandomString = new StringBuilder(Long.toString(pseudoRandomNumber)).reverse().toString();
+            for (char digit : pseudoRandomString.toCharArray()) {
+                if (secretCode.isEmpty() && digit == '0') {
+                    continue;
+                }
+                if(!usedDigits.contains(digit) && Character.isDigit(digit)) {
+                    secretCode.append(digit);
+                    usedDigits.add(digit);
+                    if(secretCode.length() == length) {
+                        break;
+                    }
+                }
+            }
+        }
+        return secretCode.toString();
+    }
+
+    private static void gradeGuess(String secretCode, String guess) {
         int bulls = 0;
         int cows = 0;
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < secretCode.length(); i++) {
             if (guess.charAt(i) == secretCode.charAt(i)) {
                 bulls++;
             } else if (secretCode.contains(String.valueOf(guess.charAt(i)))) {
@@ -27,5 +60,4 @@ public class Main {
             System.out.println("Grade: " + bulls + " bull(s) and " + cows + " cow(s). The secret code is " + secretCode + ".");
         }
     }
-
 }
